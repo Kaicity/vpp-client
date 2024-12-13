e623d45dac038c8d15a1e13064ff181e5913334f
Nguyễn Minh Thông <80609391+Kaicity@users.noreply.github.com>
2024-12-14 00:58:18 +0700

    Update CategoryList.tsx

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -9,9 +9,13 @@ interface catalogProps {
 const CategoryList: React.FC<catalogProps> = ({ catalogs }) => {
   return (
     <div className="px-4">
-      <div className="flex gap-4 md:gap-8">
+      <div className="flex gap-4 md:gap-8 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
         {catalogs.map((catalog) => (
-          <Link key={catalog.id} href={`/list`} className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+          <Link
+            key={catalog.id}
+            href={`/list`}
+            className="flex-shrink-0 w-64 sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          >
             <div className="relative bg-slate-100 w-full h-96">
               <Image src={catalog?.imageUrl || ProductDefault} alt="" fill sizes="20vw" className="object-cover" />
             </div>
