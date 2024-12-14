b55383c78e4a3d2ecbdb870c72e7daa7cb30121c
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2024-12-14 11:26:56 +0700

    fix-bug-1.4

--- src/components/ModalDialog.tsx
+++ src/components/ModalDialog.tsx
@@ -27,7 +27,7 @@ const ModalDialog: React.FC<Props> = ({
   icon,
 }) => {
   return (
-    <Dialog open={open} onClose={() => setOpen(false)} className="relative z-10">
+    <Dialog open={open} onClose={() => setOpen(false)} className="relative z-10" static>
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
